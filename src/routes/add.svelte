d598be94c56df90e8b5808537ca1370b100504cc
zrecovery <zrecovery@outlook.com>
2022-04-05 22:18:46 +0800

    feat: 新增录入文章的表单

--- src/routes/add.svelte
+++ src/routes/add.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { articlesDBStoreService } from '$lib/articles/articlesStoreService';
-	import type { Article } from '$lib/articles/articles';
+	import { Article } from '$lib/articles/articles';
 
-    let files: FileList;
-
+	let files: FileList;
+	let article = new Article('', '', '', 1.0, '');
 	async function saveArticleFromJSON(jsonFILE: File) {
 		let articles: Article[] = [];
 		try {
@@ -15,11 +15,23 @@
 		}
 	}
 
-    $: if (files) {
+	$: if (files) {
 		let fileList = Array.from(files);
 		Promise.all(fileList.map(saveArticleFromJSON));
 	}
+	async function save() {
+		await articlesDBStoreService.addArticles([article]);
+	}
 </script>
+
+<form>
+	<label>标题：<input type="text" placeholder="标题" bind:value={article.title} /></label>
+	<label>作者：<input type="text" placeholder="作者" bind:value={article.author} /></label>
+	<label>系列：<input type="text" placeholder="系列" bind:value={article.book} /></label>
+	<label>顺序：<input type="number" placeholder="顺序" bind:value={article.serial} /></label>
+	<label>内容：<textarea bind:value={article.content} /></label>
+	<button on:click={save}>保存</button>
+</form>
 
 <div>
 	<input accept="application/json" type="file" bind:files id="files" />
