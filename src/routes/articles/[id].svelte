f3a93f9463f26d60f0d4e1acdbe03962815421fc
zrecovery <zrecovery@outlook.com>
2022-04-05 21:15:35 +0800

    feat: 关键词搜索跳转功能

--- src/routes/articles/[id].svelte
+++ src/routes/articles/[id].svelte
@@ -7,11 +7,18 @@
 	let article = liveQuery(async () => {
 		return await articlesDBStoreService.getArticleByID(ID);
 	});
+	
+	let keyword: string;
+	function find() {
+		globalThis.find(keyword, false, false, true, false, true, true);
+	}
 </script>
-{#if $article}
 
-<article>
-	<h1>{$article.title}</h1>
-	<p style="white-space: pre-line;">{$article.content}</p>
-</article>
+{#if $article}
+	<input bind:value={keyword}/>
+	<button on:click={find} >搜索</button>
+	<article>
+		<h1>{$article.title}</h1>
+		<p style="white-space: pre-line;">{$article.content}</p>
+	</article>
 {/if}
