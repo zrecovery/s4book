2e6b09063d07eb3764ebd2ca2a3d63ec50d58e7a
zrecovery <zrecovery@outlook.com>
2022-04-09 20:14:30 +0800

    feat: 新增搜索栏和查找栏组件

---
+++ src/lib/components/Findbar/findbar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let keyword: string;
+	function find() {
+		globalThis.find(keyword, false, false, true, false, true, true);
+	}
+</script>
+
+<div class="row g-3">
+	<div class="col-auto">
+		<input class="form-control col-sm-7" type="text" bind:value={keyword} />
+	</div>
+	<div class="col-auto">
+		<button class="btn btn-primary" on:click={find}>搜索</button>
+	</div>
+</div>
