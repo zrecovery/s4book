14c3b3035fba447762edbf6f99e6300fd88147bf
zrecovery <zrecovery@outlook.com>
2022-04-16 20:48:58 +0800

    fix: 修复在iOS下PWA查找功能异常

--- src/lib/components/Findbar/findbar.svelte
+++ src/lib/components/Findbar/findbar.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	let keyword: string;
+	let keyword: string = '';
+	// 在iOS下PWA中，搜索关键词时，由于查找栏位于顶部，会强制聚焦于用户输入的关键词，导致无法搜索正文部分的关键词。
+	let cache = '';
 	function find() {
-		globalThis.find(keyword, false, false, true, false, true, true);
+		// 通过先缓存用户输入，再清空用户输入，保证搜索时不会聚焦至用户输入的关键词。
+		if (keyword !== '') {
+			cache = keyword;
+			keyword = '';
+		}
+		// find不是标准的一部分，属于浏览器私有。
+		globalThis.find(cache, false, false, true, false, true, true);
 	}
 </script>
 
