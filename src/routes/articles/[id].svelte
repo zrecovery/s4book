178b5172229cbce18521fa5cf47e2e8213c9ba24
zrecovery <zrecovery@outlook.com>
2022-04-17 21:48:53 +0800

    feat: 限制全局最大宽度

--- src/routes/articles/[id].svelte
+++ src/routes/articles/[id].svelte
@@ -43,34 +43,32 @@
 </script>
 
 <svelte:head><title>{article.title}</title></svelte:head>
-<div style="max-width: 95vw;">
-	{#if mode === Mode.Read}
-		<div class="m-2">
-			{#if article}
-				<article>
-					<h1>{article.title}</h1>
-					<p style="white-space: pre-line;">{article.content}</p>
-				</article>
-			{/if}
-		</div>
-		<button
-			type="button"
-			class="btn btn-primary col-2 m-2"
-			on:click={() => {
-				mode = Mode.Edit;
-			}}>编辑</button
-		>
-		<button type="button" class="btn btn-danger col-2 m-2" on:click={remove}>删除</button>
-	{:else}
-		<button
-			type="button"
-			class="btn btn-primary"
-			on:click={() => {
-				mode = Mode.Read;
-			}}>返回</button
-		>
+{#if mode === Mode.Read}
+	<div class="m-2">
+		{#if article}
+			<article>
+				<h1>{article.title}</h1>
+				<p style="white-space: pre-line;">{article.content}</p>
+			</article>
+		{/if}
+	</div>
+	<button
+		type="button"
+		class="btn btn-primary col-2 m-2"
+		on:click={() => {
+			mode = Mode.Edit;
+		}}>编辑</button
+	>
+	<button type="button" class="btn btn-danger col-2 m-2" on:click={remove}>删除</button>
+{:else}
+	<button
+		type="button"
+		class="btn btn-primary"
+		on:click={() => {
+			mode = Mode.Read;
+		}}>返回</button
+	>
 
-		<ArticleEditor bind:article />
-		<button type="button" class="btn btn-primary" on:click={update}>更改</button>
-	{/if}
-</div>
+	<ArticleEditor bind:article />
+	<button type="button" class="btn btn-primary" on:click={update}>更改</button>
+{/if}
