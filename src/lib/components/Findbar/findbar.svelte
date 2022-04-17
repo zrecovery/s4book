804811997dcdf6a84e6c6467446a0e77f92a6ea0
zrecovery <zrecovery@outlook.com>
2022-04-17 21:18:50 +0800

    fix: 确认修复在iPone下无法查找跳转关键词的问题

--- src/lib/components/Findbar/findbar.svelte
+++ src/lib/components/Findbar/findbar.svelte
@@ -2,14 +2,32 @@
 	let keyword: string = '';
 	// 在iOS下PWA中，搜索关键词时，由于查找栏位于顶部，会强制聚焦于用户输入的关键词，导致无法搜索正文部分的关键词。
 	let cache = '';
+	let count = 0;
+	let keywordTags: HTMLCollectionOf<HTMLSpanElement>;
 	function find() {
 		// 通过先缓存用户输入，再清空用户输入，保证搜索时不会聚焦至用户输入的关键词。
-		if (keyword !== '') {
-			cache = keyword;
-			keyword = '';
+		if (window.navigator.userAgent.includes('iPhone')) {
+			if (keyword !== '') {
+				cache = keyword;
+				keyword = '';
+				count = 0;
+				let articleTags = document.getElementsByTagName('article');
+				if (articleTags.length === 1) {
+					let contentElement = articleTags[0].lastChild as HTMLParagraphElement;
+					const newContentHtml = contentElement.innerHTML.replaceAll(
+						cache,
+						`<span style="background-color: yellow;">${cache}</span>`
+					);
+					contentElement.innerHTML = newContentHtml;
+					keywordTags = document.getElementsByTagName('span');
+				}
+			}
+			let index = count % keywordTags.length;
+			count = count + 1;
+			keywordTags[index].scrollIntoView({ block: 'center' });
+		} else {
+			globalThis.find(keyword, false, false, true, false, true, true);
 		}
-		// find不是标准的一部分，属于浏览器私有。
-		globalThis.find(cache, false, false, true, false, true, true);
 	}
 </script>
 
@@ -18,6 +36,12 @@
 		<input class="form-control col-sm-7" type="text" placeholder="关键词" bind:value={keyword} />
 	</div>
 	<div class="col-auto">
-		<button type="button" class="btn btn-primary" on:click={find}>搜索</button>
+		<button
+			type="button"
+			class="btn btn-primary"
+			on:click={() => {
+				find();
+			}}>搜索</button
+		>
 	</div>
 </div>
